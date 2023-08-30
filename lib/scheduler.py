14b038f1b10ed62c365099f6c110e540eb6ede56
Mmackz <matthewmackenz@gmail.com>
2023-08-29 21:25:30 -0700

    ✨ force utc timezone

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -1,13 +1,16 @@
+import pytz
 import utils.utils as utils
 from lib.reddit import find_snapshot_post
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
-from lib.constants import *
 
 # Setup scheduler
 scheduler = BackgroundScheduler(timezone="UTC")
 
+start_date = utils.calculate_snapshot_date()
+start_date = pytz.UTC.localize(start_date)
+
 scheduler.add_job(find_snapshot_post, trigger=IntervalTrigger(
-    start_date=utils.calculate_snapshot_date(),
+    start_date=start_date, 
     days=28
 ))
